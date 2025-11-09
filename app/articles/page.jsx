a9984016dbb96f9c5c56ea75b6c4724e2878511c
kort18 <kmagarde1808@gmail.com>
2025-11-09 21:51:36 +0800

    Update page.jsx

--- app/articles/page.jsx
+++ app/articles/page.jsx
@@ -161,7 +161,7 @@ export default function Page() {
 }
 
 body {
-  background-image: url('app-directory/public/BAGUIO.jpg');
+  background-image: url('app-directory/app/public/BAGUIO.jpg');
   background-size: cover;        
   background-position: center;    
   background-repeat: no-repeat;   
